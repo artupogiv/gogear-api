a8f0069a3b7ff86575392fca8bd243f0cade3ed3
Vigo Putra <artupogiv@gmail.com>
2025-04-27 06:27:48 +0700

    chore: update seeding

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -5,8 +5,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "noir-timeless-1800-96",
     name: "Noir Timeless1800 96% Wireless Mechanical Keyboard Gasket Mount",
     categorySlug: "keyboards",
-    description:
-      "Timeless 1800 – For those who want it all. 98 keys, full numpad, TFT screen, Knob and everything you need in one board",
+    description: `<p>Timeless 1800 – For those who want it all. 98 keys, full numpad, TFT screen, Knob and everything you need in one board</p>`,
     imageUrl:
       "https://ucarecdn.com/677b27bf-cc54-4164-9e88-bb07442f6984/timeless_1800Artboard_1_cd6030ba6d334cf398a63022e865d00a.jpg",
     price: 1299000,
@@ -15,8 +14,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "rexus-mouse-wireless-qv-270",
     name: "Rexus Mouse Wireless Office Clif II QV-270",
     categorySlug: "mouse",
-    description:
-      "Ergonomic vertical mouse specially designed to provide a comfortable and natural grip.",
+    description: `<p>Ergonomic vertical mouse specially designed to provide a comfortable and natural grip.</p>`,
     imageUrl:
       "https://ucarecdn.com/b8f09784-cc77-44b6-bb28-5b8a43e3fde6/Clif_2_2.jpg",
     price: 349000,
@@ -25,8 +23,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "tenjin-scrollworks-katawaguruma-deskmat",
     name: "Tenjin Scrollworks - KATAWAGURUMA Deskmat",
     categorySlug: "deskmat",
-    description:
-      "The third release of Yokai Series, featuring Katawaguruma — the burning ox cart wheel embodiment.",
+    description: `<p>The third release of Yokai Series, featuring Katawaguruma — the burning ox cart wheel embodiment.</p>`,
     imageUrl:
       "https://ucarecdn.com/17fded15-3cf1-4da9-9705-77d922c8a37a/5802d18c1f504ad58a3f25cce59f30ce.jpg",
     price: 299000,
@@ -35,8 +32,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "noir-timeless-82-v2-classic",
     name: "Noir Timeless82 v2 Classic",
     categorySlug: "keyboards",
-    description:
-      "Timeless82 V2 Classic – The entry-level keyboard that gives you everything. The perfect first keyboard, with all the features you need.",
+    description: `<p>Timeless82 V2 Classic – The entry-level keyboard that gives you everything. The perfect first keyboard, with all the features you need.</p>`,
     imageUrl:
       "https://ucarecdn.com/66345e9c-c3b5-48b8-90b0-249bbe0a0a4a/NoirTimeless82v2ClassicEdition_Black.jpg",
     price: 1199000,
@@ -45,8 +41,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "epomaker-th85-75",
     name: "Epomaker TH85 75% Gasket Hot-swappable Wireless Mechanical Keyboard",
     categorySlug: "keyboards",
-    description:
-      "The TH85 Gaming Keyboard greatly enhances gaming experience with low latency, anti-ghosting, and precise keystrokes, allowing for rapid response times during intense gameplay.",
+    description: `<p>The TH85 Gaming Keyboard greatly enhances gaming experience with low latency, anti-ghosting, and precise keystrokes, allowing for rapid response times during intense gameplay.</p>`,
     imageUrl:
       "https://ucarecdn.com/4bba700e-3991-4559-9fe5-b5b64ddae816/EpomakerTH8575GasketHotswappableWirelessMechanicalVIAGamingKeyboard.jpg",
     price: 1299000,
@@ -55,8 +50,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "tenjin-scrollworks-x-faith-industries-queen",
     name: "Tenjin Scrollworks x Faith Industries - BATTLE OF THE QUEENS Deskmat",
     categorySlug: "deskmat",
-    description:
-      "Commencing the first battle royal: Fire Heart vs Wind Diamond, featuring Fenix and Feng — incarnation of the vermillion bird and the azure dragon.",
+    description: `<p>Commencing the first battle royal: Fire Heart vs Wind Diamond, featuring Fenix and Feng — incarnation of the vermillion bird and the azure dragon.</p>`,
     imageUrl:
       "https://ucarecdn.com/8ad7b8ac-92ef-4cba-8d0a-ab0c240525fb/TenjinScrollworksxFaithIndustriesBATTLEOFTHEQUEENSDeskmat.jpg",
     price: 499000,
@@ -65,8 +59,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "tenjin-scrollworks-x-faith-industries-king",
     name: "Tenjin Scrollworks x Faith Industries - BATTLE OF THE KINGS Deskmat",
     categorySlug: "deskmat",
-    description:
-      "Commencing the second battle royal: Frost Club vs Earth Spade, featuring Frank and Fritz — incarnation of the black tortoise and the white tiger.",
+    description: `<p>Commencing the second battle royal: Frost Club vs Earth Spade, featuring Frank and Fritz — incarnation of the black tortoise and the white tiger.</p>`,
     imageUrl:
       "https://ucarecdn.com/c7313c65-e534-4f72-885a-e8d62d9ce321/TenjinScrollworksxFaithIndustriesBATTLEOFTHEKINGSDeskmat.jpg",
     price: 499000,
@@ -75,8 +68,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "rexus-mouse-gaming-ergo-dx-pw2",
     name: "Rexus Mouse Gaming Ergonomic Daxa Pro Wireless II / DX-PW2 - 8K Polling Rate PAW3395",
     categorySlug: "mouse",
-    description:
-      "​Introducing the Rexus Daxa Pro Wireless II (DX-PW2) – a premium gaming mouse that seamlessly combines precision, comfort, and style to elevate your gaming experience.",
+    description: `<p>​Introducing the Rexus Daxa Pro Wireless II (DX-PW2) – a premium gaming mouse that seamlessly combines precision, comfort, and style to elevate your gaming experience.</p>`,
     imageUrl:
       "https://ucarecdn.com/8dc8f732-ec18-4b54-b84d-2cd817829ff2/RexusMouseGamingErgonomicDaxaProWirelessIIDXPW28KPollingRatePAW3395.jpg",
     price: 689000,
@@ -85,8 +77,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "noir-m2-pro-gaming-mouse",
     name: "Noir M2 Pro - Wireless Ultra Lightweight Gaming Mouse",
     categorySlug: "mouse",
-    description:
-      "A wireless ultra-lightweight gaming mouse engineered for precision, speed, and comfort. Weighing just 59 grams, the M2 Pro is designed for gamers who demand agility without compromising performance.",
+    description: `<p>A wireless ultra-lightweight gaming mouse engineered for precision, speed, and comfort. Weighing just 59 grams, the M2 Pro is designed for gamers who demand agility without compromising performance.</p>`,
     imageUrl:
       "https://ucarecdn.com/79212731-f760-4083-bc49-de5d1ab32713/NoirM2ProWirelessUltraLightweightGamingMouse_Black.jpg",
     price: 849000,
@@ -95,8 +86,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "rexus-deskmat-daxa-land-of-light",
     name: "Rexus Deskmat Mousepad Daxa Dream - Land of Light",
     categorySlug: "deskmat",
-    description:
-      "The DAXA Dream DAXA Retro Pearl Mousemat Deskmat complements the unique motif variant of the previous Retro version. The Extra Large size is also able to accommodate a variety of mouse movements used on it. The DAXA Retro Pearl Mousemat uses a waterproof polyester material that is smooth yet contoured to minimize friction between the mousepad and mouse feet. The DAXA Retro Pearl Mousemat promises a more stylish appearance in perfect mouse movement.",
+    description: `<p>The DAXA Dream DAXA Retro Pearl Mousemat Deskmat complements the unique motif variant of the previous Retro version. The Extra Large size is also able to accommodate a variety of mouse movements used on it. The DAXA Retro Pearl Mousemat uses a waterproof polyester material that is smooth yet contoured to minimize friction between the mousepad and mouse feet. The DAXA Retro Pearl Mousemat promises a more stylish appearance in perfect mouse movement.</p>`,
     imageUrl:
       "https://ucarecdn.com/66704faa-2a7d-4fad-a57d-c96016fd51f2/RexusDeskmatMousepadDaxaDream_LandofLight.jpg",
     price: 199000,
@@ -105,8 +95,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "logitech-m720-triathlon",
     name: "Logitech M720 Triathlon",
     categorySlug: "mouse",
-    description:
-      "Meet the high-precision mouse that’s built for ultimate comfort, endurance, and versatility. With extra controls, a dual-mode scrolling, and adjustable dpi tracking, M720 gives you the power to tackle any task.",
+    description: `<p>Meet the high-precision mouse that’s built for ultimate comfort, endurance, and versatility. With extra controls, a dual-mode scrolling, and adjustable dpi tracking, M720 gives you the power to tackle any task.</p>`,
     imageUrl:
       "https://ucarecdn.com/f6e90499-d873-4454-a132-a2e5ba5d12f7/M720Triathlon.jpg",
     price: 919000,
@@ -115,8 +104,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "nuphyx-bh65",
     name: "NuPhyX BH65 Magnetic Switch Gaming Keyboard",
     categorySlug: "keyboards",
-    description:
-      "The keyboard features a shell-like structure, providing both durability and a sleek aesthetic.The integrated footrest also enhances the keyboard's stability, resulting in better performance during gaming.",
+    description: `<p>The keyboard features a shell-like structure, providing both durability and a sleek aesthetic.The integrated footrest also enhances the keyboard's stability, resulting in better performance during gaming.</p>`,
     imageUrl:
       "https://ucarecdn.com/eba3e307-8391-47a0-bf74-c74ce099e53e/NuPhyXBH65MagneticSwitchGamingKeyboard.jpg",
     price: 2699000,
@@ -125,8 +113,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "rexus-deskmat-daxa-busy-doodle",
     name: "Rexus Deskmat Mousepad Daxa Dream - Busy Doodle",
     categorySlug: "deskmat",
-    description:
-      "The DAXA Dream DAXA Retro Pearl Mousemat Deskmat complements the unique motif variant of the previous Retro version. The Extra Large size is also able to accommodate a variety of mouse movements used on it. The DAXA Retro Pearl Mousemat uses a waterproof polyester material that is smooth yet contoured to minimize friction between the mousepad and mouse feet. The DAXA Retro Pearl Mousemat promises a more stylish appearance in perfect mouse movement.",
+    description: `<p>The DAXA Dream DAXA Retro Pearl Mousemat Deskmat complements the unique motif variant of the previous Retro version. The Extra Large size is also able to accommodate a variety of mouse movements used on it. The DAXA Retro Pearl Mousemat uses a waterproof polyester material that is smooth yet contoured to minimize friction between the mousepad and mouse feet. The DAXA Retro Pearl Mousemat promises a more stylish appearance in perfect mouse movement.</p>`,
     imageUrl:
       "https://ucarecdn.com/cc5cf454-021f-4c23-8182-80ffa26bd056/RexusDeskmatMousepadDaxaDream_BussyDoodle.jpg",
     price: 199000,
@@ -135,8 +122,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "epomaker-click-mouse",
     name: "Epomaker Click Mouse",
     categorySlug: "mouse",
-    description:
-      "The Epomaker Click Mouse is wired & wireless gaming mouse with programmable side buttons that ensures a reliable gaming experience with its precise optical sensor, responsive micro-switches, and programmable side buttons for enhanced efficiency.",
+    description: `<p>The Epomaker Click Mouse is wired & wireless gaming mouse with programmable side buttons that ensures a reliable gaming experience with its precise optical sensor, responsive micro-switches, and programmable side buttons for enhanced efficiency.</p>`,
     imageUrl:
       "https://ucarecdn.com/7feb8a92-dd06-4be8-b315-487f809673e1/EpomakerClickMouseWiredWirelessGamingMousewithprogrammableSideButtons.jpg",
     price: 1069000,
@@ -145,8 +131,7 @@ export const dataProducts: CreateProduct[] = [
     slug: "keychron-k3-qmk",
     name: "Keychron K3 QMK Wireless Mechanical Keyboard",
     categorySlug: "keyboards",
-    description:
-      "The Keychron K3 Version 3 is a low-profile wireless mechanical keyboard with a 75% layout, now upgraded with QMK firmware and enhanced acoustic foams. It supports both Bluetooth 5.2 and wired connections. The Launcher web app offers limitless customization and boosts productivity for your work!",
+    description: `<p>The Keychron K3 Version 3 is a low-profile wireless mechanical keyboard with a 75% layout, now upgraded with QMK firmware and enhanced acoustic foams. It supports both Bluetooth 5.2 and wired connections. The Launcher web app offers limitless customization and boosts productivity for your work!</p>`,
     imageUrl:
       "https://ucarecdn.com/d77b1c9c-e33d-45a5-ae54-b5df906de032/KeychronK3Version3UltraSlimWirelessMechanicalKeyboardLowProfileBrownSwitchWhiteBacklight.jpg",
     price: 1499000,
